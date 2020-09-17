1dfa8c93bbc0eee29617c99a5dc71748c89d320f
Andy Tran <foolmonkey99@gmail.com>
2020-09-17 14:58:48 -0600

    Updates

---
+++ precache-manifest.7e6904e4c2b5f69a4cb0b43aeee77c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f653d98bb13930b09459ae45d8992c4",
+    "url": "/rest-country-api/index.html"
+  },
+  {
+    "revision": "2da55be7abc9d50f7760",
+    "url": "/rest-country-api/static/css/main.3212eba1.chunk.css"
+  },
+  {
+    "revision": "f34bcfbaab716df1d565",
+    "url": "/rest-country-api/static/js/2.3bcfb9a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rest-country-api/static/js/2.3bcfb9a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da55be7abc9d50f7760",
+    "url": "/rest-country-api/static/js/main.7290092b.chunk.js"
+  },
+  {
+    "revision": "adebd75532e444f00ae4",
+    "url": "/rest-country-api/static/js/runtime-main.f5859d1f.js"
+  }
+]);
